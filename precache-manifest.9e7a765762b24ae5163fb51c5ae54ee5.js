230007f8e810ea282331898735f1e21f48811b31
Pre-Dev <deveshkorde@users.noreply.github.com>
2020-10-25 14:06:16 +0530

    Updates

---
+++ precache-manifest.9e7a765762b24ae5163fb51c5ae54ee5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf0bf78b7b753ec8986fc84c12bb8a09",
+    "url": "/Devesh/index.html"
+  },
+  {
+    "revision": "b686a5967e664d5ee050",
+    "url": "/Devesh/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d59bea74311f4f79b78a",
+    "url": "/Devesh/static/css/main.90be5137.chunk.css"
+  },
+  {
+    "revision": "b686a5967e664d5ee050",
+    "url": "/Devesh/static/js/2.7f20ca75.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Devesh/static/js/2.7f20ca75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d59bea74311f4f79b78a",
+    "url": "/Devesh/static/js/main.d3a4445a.chunk.js"
+  },
+  {
+    "revision": "fad679d8d5b7499e8568",
+    "url": "/Devesh/static/js/runtime-main.bbeaaf5a.js"
+  }
+]);
