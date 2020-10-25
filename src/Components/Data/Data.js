1fed8f533c40c0f2a9fb4fcbbb5c93d51bd0151c
Pre-Dev <deveshkorde@users.noreply.github.com>
2020-10-25 15:12:44 +0530

    s

--- src/Components/Data/Data.js
+++ src/Components/Data/Data.js
@@ -5,7 +5,7 @@ const Data = ({ onHireMeClick }) => {
     return (
         <div className="data">
             <div className="data_image">
-            <img alt="Laptop" href="%PUBLIC_URL%/Images/gog.png" height="auto" width="450px"></img>
+            <link rel="apple-touch-icon" href="%PUBLIC_URL%/Images/gog.png" />
             </div>
             <div className="data_info">
                 <p>
