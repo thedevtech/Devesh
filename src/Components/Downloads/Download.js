79d3277e88306c7b916c6f7412102b89393595b9
Pre-Dev <deveshkorde@users.noreply.github.com>
2020-11-05 08:04:55 +0530

    Eloquent Javascript

--- src/Components/Downloads/Download.js
+++ src/Components/Downloads/Download.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Download.css';
 import Cpp from './cpp.pdf';
+import Javascript from './Javascript.pdf';
 
 const Downloads = () => {
     return (
@@ -18,6 +19,21 @@ const Downloads = () => {
                            <li>Simple and sweet</li>
                        </ul> </p>
                     <button className="downloads_button"><a href={Cpp} download="C++.pdf" className="downloads_link">Download</a></button>
+               </div>
+           </div>
+           <div className="downloads_margin">
+               <div className="downloads">
+                    <h1 className="downloads_heading">Eloquent Javascript</h1>
+                    <p className="downloads_para">A guide for all Javascript Programmers</p>
+                    <p className="downloads_para">
+                       <ul type="dot" className="downloads_list">
+                           <li> Book by Marijn Harverbeke</li>
+                           <li>Third Edition</li>
+                           <li>Everything Explained in a detailed way</li>
+                           <li>Good examples, good explantion</li>
+                           <li>Javascript Lovers will love this book</li>
+                       </ul> </p>
+                    <button className="downloads_button"><a href={Javascript} download="Eloquent Javascript.pdf" className="downloads_link">Download</a></button>
                </div>
            </div>
        </div>
