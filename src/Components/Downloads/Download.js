328cddeb469ceac45ad5f35d6541c19297fd0792
Pre-Dev <deveshkorde@users.noreply.github.com>
2020-11-06 18:05:17 +0530

    making code clean

--- src/Components/Downloads/Download.js
+++ src/Components/Downloads/Download.js
@@ -3,40 +3,46 @@ import './Download.css';
 import Cpp from './cpp.pdf';
 import Javascript from './Javascript.pdf';
 
+
+
 const Downloads = () => {
+    const Data = [
+        {
+            id:1,
+            bookName:'C++ Programming',
+            bookLine:'A guide for all Cpp Programmers',
+            point1:'Book by Wouter Verkerke (NIKHEF)',
+            point2:'Concepts explained in a very good way',
+            point3:'Presentation Type of books',
+            point4:'Perfect use of arrows, good explantion',
+            bookLink:Cpp,
+           
+        },
+        {
+            id:2,
+            bookName:'Eloquent Javascript',
+            bookLine:'A guide for all Javascript Programmers',
+            bookdescription:'This is the book by Marijn Harverbeke which is Third Edition. In this book everything is explained in a detailed way. This book contains good examples & good explantions.',
+            bookLink:Javascript,
+           
+        }
+    ]
+    const renderList = Data.map((items)=>{
+        return(      
+                <div className="downloads_container">
+                    <div className="downloads_margin">
+                        <div className="downloads"> 
+                         <h1 className="downloads_heading">{items.bookName}</h1>
+                         <p className="downloads_para"><b>{items.bookLine}</b></p>
+                         <p className="downloads_para">{items.bookdescription}</p>
+                         <button className="downloads_button"><a href={items.bookLink} download="C++.pdf" className="downloads_link">Download</a></button>
+                        </div>
+                    </div>
+                </div>
+        );
+     })
     return (
-       <div className="downloads_container">
-           <div className="downloads_margin">
-               <div className="downloads">
-                    <h1 className="downloads_heading">C++ Programming</h1>
-                    <p className="downloads_para">A guide for all Cpp Programmers</p>
-                    <p className="downloads_para">
-                       <ul type="dot" className="downloads_list">
-                           <li> Book by Wouter Verkerke (NIKHEF)</li>
-                           <li>Concepts explained in a very good way</li>
-                           <li>Presentation Type of books</li>
-                           <li>Perfect use of arrows, good explantion</li>
-                           <li>Simple and sweet</li>
-                       </ul> </p>
-                    <button className="downloads_button"><a href={Cpp} download="C++.pdf" className="downloads_link">Download</a></button>
-               </div>
-           </div>
-           <div className="downloads_margin">
-               <div className="downloads">
-                    <h1 className="downloads_heading">Eloquent Javascript</h1>
-                    <p className="downloads_para">A guide for all Javascript Programmers</p>
-                    <p className="downloads_para">
-                       <ul type="dot" className="downloads_list">
-                           <li> Book by Marijn Harverbeke</li>
-                           <li>Third Edition</li>
-                           <li>Everything Explained in a detailed way</li>
-                           <li>Good examples, good explantion</li>
-                           <li>Javascript Lovers will love this book</li>
-                       </ul> </p>
-                    <button className="downloads_button"><a href={Javascript} download="Eloquent Javascript.pdf" className="downloads_link">Download</a></button>
-               </div>
-           </div>
-       </div>
+       renderList
     );
 }
 
