669ca9c402ecdf4a5cfec2a6650291037e3a088e
Pre-Dev <deveshkorde@users.noreply.github.com>
2020-11-08 19:21:23 +0530

    Updates

---
+++ precache-manifest.63e859fa5d9ab62a439690127a0e72f7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "488534aa3510f7caa5ce82b6872a1d0b",
+    "url": "/Devesh/index.html"
+  },
+  {
+    "revision": "9986543fbfd5aea1cd49",
+    "url": "/Devesh/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6afb3ddb832228304f06",
+    "url": "/Devesh/static/css/main.aa0b891f.chunk.css"
+  },
+  {
+    "revision": "9986543fbfd5aea1cd49",
+    "url": "/Devesh/static/js/2.18a702e8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Devesh/static/js/2.18a702e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6afb3ddb832228304f06",
+    "url": "/Devesh/static/js/main.c402d16d.chunk.js"
+  },
+  {
+    "revision": "fad679d8d5b7499e8568",
+    "url": "/Devesh/static/js/runtime-main.bbeaaf5a.js"
+  },
+  {
+    "revision": "4d2413a92f28d56babb80d352cd4f6da",
+    "url": "/Devesh/static/media/Javascript.4d2413a9.pdf"
+  },
+  {
+    "revision": "275f1f771e7863908607a67acb0e72d5",
+    "url": "/Devesh/static/media/Stackoverflow C++.275f1f77.pdf"
+  },
+  {
+    "revision": "c39b9942f0383a0b40435126f56b68b0",
+    "url": "/Devesh/static/media/cpp.c39b9942.pdf"
+  },
+  {
+    "revision": "80218fc0f5f1c3d1fe258157068922c4",
+    "url": "/Devesh/static/media/devesh.80218fc0.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Devesh/precache-manifest.9979368ec601a30cf339edb1c8cc93a1.js"
+  "/Devesh/precache-manifest.63e859fa5d9ab62a439690127a0e72f7.js"
 );
 
 self.addEventListener('message', (event) => {
