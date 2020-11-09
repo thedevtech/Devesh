4cab7ddc551a9cdc11754fef1a4802aa44174e30
Pre-Dev <deveshkorde@users.noreply.github.com>
2020-11-09 12:53:51 +0530

    Blog added

--- src/Components/Blogs/Blog.js
+++ src/Components/Blogs/Blog.js
@@ -1,46 +1,19 @@
 import React from 'react';
 import './Blog.css';
+import dvl from './DvL.png';
 
 const Blog = () => {
     return (
         <div className="temporary">
-            
-                Yet To Come !
-           
-        </div>
-  /*      <div className="blog">
+            <div className="blog">
             <div className="blog_container blog_container1">
-                <img alt="blog1" src="/Images/blog.jpg" className="blog_image"></img>
-               <h1 className="blog_heading">Five methods for binding this in react!</h1>
-               <p>Discription</p>
-               <button  className="blog_button">View Blog</button>
-               <button  className="blog_button_center">View Github Link</button>
-               <button  className="blog_button"> View Blog Example</button>
+                <img alt="blog1" src={dvl} className="blog_image"></img>
+               <h1 className="blog_heading">Dark Mode v/s Light Mode</h1>
+               <p>This blog elaborates the difference between dark and light mode. The blog is written on the DEV.to platform.</p>
+               <button  className="blog_button"><a  className="blog_link" href="https://dev.to/deveshkorde/why-do-a-dark-theme-is-best-than-the-ordinary-one-3928">View Blog</a></button>
             </div>
-            <div className=" blog_container">
-            <img alt="blog2" src="/Images/blog2.jpg" className="blog_image"></img>
-               <h1 className="blog_heading">Five methods for binding this in react!</h1>
-               <p>Discription</p>
-               <button  className="blog_button">View Blog</button>
-               <button  className="blog_button_center">View Github Link</button>
-               <button  className="blog_button"> View Blog Example</button>
             </div>
-            <div className=" blog_container">
-               <h1 className="blog_heading">Five methods for binding this in react!</h1>
-               <p>Discription</p>
-               <button  className="blog_button">View Blog</button>
-               <button  className="blog_button_center">View Github Link</button>
-               <button  className="blog_button"> View Blog Example</button>
-            </div>
-            <div className=" blog_container">
-               <h1 className="blog_heading">Five methods for binding this in react!</h1>
-               <p>Discription</p>
-               <button  className="blog_button">View Blog</button>
-               <button  className="blog_button_center">View Github Link</button>
-               <button  className="blog_button"> View Blog Example</button>
-            </div>
-        </div>
-        */
+        </div>     
     );
 }
 
